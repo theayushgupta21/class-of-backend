3a0319923c61b2b6f347d291eda04aac090af9a3
Ayush Gupta <ayushgupta9510@gmail.com>
2026-02-19 23:06:56 +0530

    second commit

--- script.js
+++ script.js
@@ -2,4 +2,16 @@
 // arrays and objects
 // functions  return
 // async js coding
-alert("Hello, World!");
+// foreach map filter find indexof
+
+var arr = [1, 2, 3, 4, 5];
+
+/* arr.forEach(function (val) {
+    console.log(val + " hello ");
+
+}) */
+var ans = arr.map(function (val) {
+    return 13;
+
+
+})
